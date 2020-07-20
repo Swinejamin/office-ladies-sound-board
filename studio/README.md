28c270ecaa19c10ae736156d206f6e0f520982c7
Sanity.io <hello@sanity.io>
2020-07-20 09:57:08 -0500

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+# office-ladies-sound-board-studio
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GRIDSOME_SANITY_PROJECT_ID || 'htjej3fm',
+    dataset: process.env.GRIDSOME_SANITY_DATASET || 'production'
+  }
+}
